d00d38346026ac998e16495c1af2a8f519f22df9
Benno <bennokolar@gmail.com>
2023-08-16 21:12:29 +0200

    Double detected Arrows get filtered out

--- main.py
+++ main.py
@@ -46,7 +46,6 @@ def enhanceImage(image_path):
 
 
 def detectRectangles(image):
-
     # Convert the image to binary using thresholding
     _, binary_image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY)
 
@@ -56,6 +55,7 @@ def detectRectangles(image):
     # Convert to color image
     color_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
+    rectangles = []
     # Iterate through the contours and draw rectangles with colored outlines
     for contour in contours:
         # OG: 0.04
@@ -66,14 +66,14 @@ def detectRectangles(image):
         if len(approx) == 4:
             x, y, w, h = cv2.boundingRect(contour)
 
-            if 30 <= w <= image.shape[1]*0.9 and 30 <= h <= image.shape[0]*0.9:
+            if 30 <= w <= image.shape[1] * 0.9 and 30 <= h <= image.shape[0] * 0.9:
+                rectangles.append(((x, y), (x + w, y + h)))
                 cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Draw green rectangle
 
-    return color_image
+    return color_image, rectangles
 
 
 def detectArrows(image):
-
     # Convert the image to binary using thresholding
     _, binary_image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY)
 
@@ -83,6 +83,7 @@ def detectArrows(image):
     # Convert to color image
     color_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
+    triangles = []
     # Iterate through the contours and draw rectangles with colored outlines
     for contour in contours:
         # OG: 0.04
@@ -92,35 +93,39 @@ def detectArrows(image):
         # 3 detects arrows? 10 detects plus signs?
         if len(approx) == 3:
             x, y, w, h = cv2.boundingRect(contour)
-
+            triangles.append(((x, y), (x + w, y + h)))
             cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 0, 255), 2)  # Draw green rectangle
 
-    return color_image
+    return color_image, triangles
+
 
 def detectSimpleArrows(image):
-    # Convert the image to binary using thresholding
-    _, binary_image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY)
+    blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
 
-    # Find contours in the binary image
-    contours, _ = cv2.findContours(binary_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # Perform edge detection using the Canny edge detector
+    edges = cv2.Canny(blurred_image, 50, 150)
 
-    # Convert to color image
-    color_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    # Perform Hough Line Transform to detect lines
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=10, minLineLength=10, maxLineGap=5)
 
-    # Iterate through the contours and draw rectangles with colored outlines
-    for i, contour in enumerate(contours):
-        #print(contour)
-        # OG: 0.04
-        epsilon = 0.04 * cv2.arcLength(contour, True)
-        approx = cv2.approxPolyDP(contour, epsilon, True)
+    # Extract starting and ending points of detected lines
+    line_points = []
+    marked_image = cv2.cvtColor(blurred_image, cv2.COLOR_GRAY2BGR)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
 
-        # 3 detects arrows? 10 detects plus signs?
-        if True or len(approx) == 2:
-            x, y, w, h = cv2.boundingRect(contour)
+            angle_deg = abs(np.degrees(np.arctan2(y2 - y1, x2 - x1)))  # Calculate angle in degrees
 
-            cv2.rectangle(color_image, (x, y), (x + w, y + h), (i*100, 0, 255), 2)  # Draw green rectangle
+            # Check if the angle falls within the desired ranges
+            range_deg = 5
+            if not (
+                    0 <= angle_deg <= range_deg or 90 - range_deg <= angle_deg <= 90 + range_deg or 180 - range_deg <= angle_deg <= 180 + range_deg or
+                    270 - range_deg <= angle_deg <= 290 + range_deg or 360 - range_deg <= angle_deg <= 360):
+                line_points.append(((x1, y1), (x2, y2)))
+                cv2.line(marked_image, (x1, y1), (x2, y2), (255, 255, 0), 2)  # Draw line in blue
+    return marked_image, line_points
 
-    return color_image
 
 def detectLines(image):
     blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
@@ -129,32 +134,52 @@ def detectLines(image):
     edges = cv2.Canny(blurred_image, 50, 150)
 
     # Perform Hough Line Transform to detect lines
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold=10, minLineLength=10, maxLineGap=5)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=20, minLineLength=30, maxLineGap=2)
 
     # Extract starting and ending points of detected lines
     line_points = []
-    marked_image=cv2.cvtColor(blurred_image, cv2.COLOR_GRAY2BGR)
+    marked_image = cv2.cvtColor(blurred_image, cv2.COLOR_GRAY2BGR)
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line[0]
 
             angle_deg = abs(np.degrees(np.arctan2(y2 - y1, x2 - x1)))  # Calculate angle in degrees
-            print(f"from ({x1} | {y1}) to ({x2} | {y2}) with slope {angle_deg}")
             # Check if the angle falls within the desired ranges
-            range_deg = 5
-            if not (0 <= angle_deg <= range_deg or 90-range_deg <= angle_deg <= 90+range_deg or 180-range_deg <= angle_deg <= 180+range_deg or
-                    270-range_deg <= angle_deg <= 290+range_deg or 360-range_deg <= angle_deg <= 360):
+            range_deg = 2
+            if (
+                    0 <= angle_deg <= range_deg or 90 - range_deg <= angle_deg <= 90 + range_deg or 180 - range_deg <= angle_deg <= 180 + range_deg or
+                    270 - range_deg <= angle_deg <= 290 + range_deg or 360 - range_deg <= angle_deg <= 360):
                 line_points.append(((x1, y1), (x2, y2)))
-                cv2.line(marked_image, (x1, y1), (x2, y2), (255, 255, 0), 2)  # Draw line in blue
-            else:
-                range_deg = 2
-                if (0 <= angle_deg <= range_deg or 90 - range_deg <= angle_deg <= 90 + range_deg or 180 - range_deg <= angle_deg <= 180 + range_deg or
-                        270 - range_deg <= angle_deg <= 290 + range_deg or 360 - range_deg <= angle_deg <= 360):
-                    line_points.append(((x1, y1), (x2, y2)))
-                    cv2.line(marked_image, (x1, y1), (x2, y2), (255, 255, 0), 2)  # Draw line in blue
                 cv2.line(marked_image, (x1, y1), (x2, y2), (0, 255, 255), 2)  # Draw line in blue
 
-    return marked_image
+    return marked_image, line_points
+
+
+def filterDoubledArrows(triangles, slopes):
+    filteredSlopes = []
+    tolerance = 100
+    for ((x1, y1), (x2, y2)) in slopes:
+        within = False
+        for ((mx1, my1), (mx2, my2)) in triangles:
+            if (
+                    ((mx1 + tolerance < x1 < mx2 - tolerance) or (mx1 - tolerance < x1 < mx2 + tolerance) or (
+                            mx1 + tolerance > x1 > mx2 - tolerance) or (mx1 - tolerance > x1 > mx2 + tolerance)) and (
+                    (my1 + tolerance < y1 < my2 - tolerance) or (my1 - tolerance < y1 < my2 + tolerance) or (
+                    my1 + tolerance > y1 > my2 - tolerance) or (my1 - tolerance > y1 > my2 + tolerance))
+            ):
+                print(f"({x1} | {y1}) and ({x2} | {y2}) is within ({mx1} | {my1}) and ({mx2} | {my2})")
+                within = True
+                break
+        if not within:
+            filteredSlopes.append(((x1, y1), (x2, y2)))
+    return filteredSlopes
+
+
+def paintLines(points, image):
+    colored_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    for ((x1, y1), (x2, y2)) in points:
+        cv2.line(colored_image, (x1, y1), (x2, y2), (255, 0, 255), 2)  # Draw line in blue
+    return colored_image
 
 
 # Main Method
@@ -165,9 +190,18 @@ if __name__ == '__main__':
 
     # Show Image
     cv2.imshow("Enhanced Image", scale(enhancedImage, 1000, 1000))
-    cv2.imshow("Rectangles", scale(detectRectangles(enhancedImage), 1000, 1000))
-    cv2.imshow("Arrows", scale(detectArrows(enhancedImage), 1000, 1000))
-    #cv2.imshow("Simple Arrows", scale(detectSimpleArrows(enhancedImage), 1000, 1000))
-    cv2.imshow("Lines", scale(detectLines(enhancedImage), 1000, 1000))
+    rectangleImage, rectangles = detectRectangles(enhancedImage)
+    cv2.imshow("Rectangles", scale(rectangleImage, 1000, 1000))
+    triangleImage, triangles = detectArrows(enhancedImage)
+    cv2.imshow("Arrows", scale(triangleImage, 1000, 1000))
+    slopeLinesImage, slopeLines = detectSimpleArrows(enhancedImage)
+    cv2.imshow("Filtered Arrows",
+               scale(paintLines(filterDoubledArrows(triangles, slopeLines), enhancedImage), 1000, 1000))
+    cv2.imshow("Simple Arrows", scale(slopeLinesImage, 1000, 1000))
+    horizontalVerticalLinesImage, horizontalVerticalLines = detectLines(enhancedImage)
+    cv2.imshow("Lines", scale(horizontalVerticalLinesImage, 1000, 1000))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+# Top Left to bottom right?
