b1f8f79de896664bb4db64070f3998f1dc8d9a38
Benno <bennokolar@gmail.com>
2023-09-06 11:09:05 +0200

    Added Java Code generation ???

--- generateCode.py
+++ generateCode.py
@@ -46,3 +46,57 @@ def generateCode(classes, inheritance):
                 actualClass["inheritsFrom"] = parent
 
     print(actualClasses)
+
+    javaCodes = []
+    for actualClass in actualClasses:
+        javaCode = json_to_java_class(actualClass)
+        print(javaCode)
+        javaCodes.append(javaCode)
+        write_java_class_to_file(javaCode, actualClass["name"])
+
+
+def json_to_java_class(json_obj):
+    # Ensure the input is a dictionary
+    if not isinstance(json_obj, dict):
+        raise ValueError("Input must be a dictionary")
+
+    # Get class name
+    class_name = json_obj.get('name', 'UnknownClass')
+
+    # Initialize the Java class code
+    java_class_code = f'public class {class_name} '
+
+    # Check if the class inherits from another class
+    inherits_from = json_obj.get('inheritsFrom', None)
+    if inherits_from:
+        java_class_code += f'extends {inherits_from} '
+
+    java_class_code += '{\n'
+
+    # Process attributes
+    attributes = json_obj.get('attributes', [])
+    for attribute in attributes:
+        attr_name = attribute.get('name', 'unknownAttribute')
+        attr_type = attribute.get('type', 'Object')
+        java_class_code += f'    private {attr_type} {attr_name};\n'
+
+    java_class_code += '\n'
+
+    # Process methods
+    methods = json_obj.get('methods', [])
+    for method in methods:
+        method_name = method.get('name', 'unknownMethod')
+        return_type = method.get('returnType', 'void')
+        arguments = ', '.join(method.get('arguments', []))
+        java_class_code += f'    public {return_type} {method_name}({arguments})' + ' {\n'
+        java_class_code += '        // Your method implementation here\n'
+        java_class_code += '    }\n\n'
+
+    java_class_code += '}'
+
+    return java_class_code
+
+
+def write_java_class_to_file(java_class_code, file_name):
+    with open(file_name, 'w') as java_file:
+        java_file.write(java_class_code)
